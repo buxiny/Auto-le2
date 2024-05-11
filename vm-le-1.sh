bb42440d34b5d601d0e40feb8d6993b082d55d72
buxiny <84549314+buxiny@users.noreply.github.com>
2024-05-11 15:33:55 +0800

    Update vm-le-1.sh

--- vm-le-1.sh
+++ vm-le-1.sh
@@ -17,8 +17,8 @@
 #sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git turboacc https://github.com/chenmozhijin/turboacc' >>feeds.conf.default
